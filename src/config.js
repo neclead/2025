5f3ad2392429e923f591489ca3dd6715897d58ae
polaroiddosa <polaroiddosa@gmail.com>
2026-01-19 14:34:50 +0530

    config: Update GOOGLE_SCRIPT_URL with user deployment

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 // Configuration for External Services
 
 // 1. Google Apps Script URL (For submitting data)
-// Follow SETUP_GUIDE.md to generate this.
-export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx_PLACEHOLDER_FOR_USER_TO_FILL/exec";
+export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzF2IH407rAwJFJUhU3h38jPkTxzum_7IM1uCNYgv1tdSgG2O5bdffYEpOrQEMrBHs1/exec";
 
 // 2. Google Sheet CSV URL (For viewing data in Dashboard)
 // Follow SETUP_GUIDE.md to generate this.
 export const GOOGLE_SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1v_PLACEHOLDER_FOR_USER_TO_FILL/pub?output=csv";
+
