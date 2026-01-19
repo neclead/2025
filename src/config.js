3f314961432cfd32e6b1d90ff400e0ae271ff650
polaroiddosa <polaroiddosa@gmail.com>
2026-01-19 14:36:57 +0530

    config: Update GOOGLE_SHEET_CSV_URL with user publication

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@
 export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzF2IH407rAwJFJUhU3h38jPkTxzum_7IM1uCNYgv1tdSgG2O5bdffYEpOrQEMrBHs1/exec";
 
 // 2. Google Sheet CSV URL (For viewing data in Dashboard)
-// Follow SETUP_GUIDE.md to generate this.
-export const GOOGLE_SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1v_PLACEHOLDER_FOR_USER_TO_FILL/pub?output=csv";
+export const GOOGLE_SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQb6iW6LNKKM4weZorBM4rY-shbjMH1ifD5CQ1MVIlJ6MiUGhwTgzEU4AEE8TWzVIwfqOH3zJrzmtfX/pub?output=csv";
+
 
